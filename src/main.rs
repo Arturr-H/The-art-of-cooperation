58cbc70a78a3166f70011394a3cb7c182b9a7ab0
Artur <artur.h.0427@gmail.com>
2023-01-09 20:56:15 +0100

    Implement dotenv varaibles for account manager

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,18 @@
 /* Imports */
 mod pixel;
 mod board;
+mod auth;
 
 use pixel::*;
 use board::*;
 
 use responder::prelude::*;
-
-use std::{fs::File, io::Read};
+use std::{ fs::File, io::Read, env::var };
 use std::path::Path;
 
 use bincode::{ config::legacy, Encode, Decode };
+use dotenv::dotenv;
+use lazy_static::lazy_static;
 
 /* Constants */
 const COLORS: &[&'static str] = &[
@@ -22,8 +24,16 @@ const COLORS: &[&'static str] = &[
     "#493fb0", "#8e68d9", "#e4abff", "#b44ac0", "#721b8c", "#ba0d8b", "#ff5392", "#ffbfbf",
     "#ffffff", "#d4d7d9", "#898d90", "#515252", "#000000", "#5c3921", "#9c6926", "#ffb470"
 ];
+lazy_static! {
+    pub static ref ACCOUNT_MANAGER_URL:String = var("ACCOUNT_MANAGER_URL").unwrap();
+}
 
 /* Initialize */
 fn main() -> () {
-    dbg!(Pixel::get_history());
+    dotenv().unwrap();
+
+    /* Load ENV variables and pre-warn about non-existence */
+    var("ACCOUNT_MANAGER_URL").unwrap();
+
+    let mut _board = Board::from_save().unwrap();
 }
