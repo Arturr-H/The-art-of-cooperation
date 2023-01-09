e345179c8abedf42f440517f6b971fb1f8f70f42
Artur <artur.h.0427@gmail.com>
2023-01-09 11:22:57 +0100

    Implement saving / retrieve methods for `Board`

--- src/board.rs
+++ src/board.rs
@@ -1,13 +1,15 @@
 /* Imports */
 use crate::pixel::{ Color, Pixel };
-use bincode::{ Encode, Decode };
+use bincode::{ Encode, Decode, config::legacy };
+use std::{fs::{OpenOptions, File}, io::{self, Write, Read}};
 
 /* Constants */
 pub const SIDE:usize = 100;
 pub const SIZE:usize = SIDE*SIDE;
+const PATH_TO_SAVE:&'static str = "./board.bin";
 
 /* Board */
-#[derive(Encode, Decode)]
+#[derive(Encode, Decode, Debug)]
 pub struct Board {
     /* One-dim board for better performance */
     pixels: [Option<Pixel>; SIZE]
@@ -15,8 +17,11 @@ pub struct Board {
 
 /* Method implementations */
 impl Board {
-    pub fn new() -> Board {
-        Board { pixels: [None; SIZE] }
+    pub fn new() -> Self {
+        Self { pixels: [None; SIZE] }
+    }
+    pub fn from_pixels(pixels:[Option<Pixel>; SIZE]) -> Self {
+        Self { pixels }
     }
     pub fn get(&self, x:usize, y:usize) -> Option<&Pixel> {
         self.pixels.get(SIDE*y + x).and_then(|i| i.as_ref())
@@ -28,5 +33,44 @@ impl Board {
 
     pub fn get_pixels(&self) -> [Option<Pixel>; SIZE] {
         self.pixels
+    }
+
+    /* Encode / decode */
+    pub fn encode(&self) -> Option<Vec<u8>> {
+        bincode::encode_to_vec(&self.pixels, legacy().with_variable_int_encoding()).ok()
+    }
+    pub fn decode(slice:&[u8]) -> Option<Self> {
+        match bincode::decode_from_slice::<[Option<Pixel>; SIZE], _>(slice, legacy().with_variable_int_encoding()) {
+            Ok(e) => Some(Self::from_pixels(e.0)),
+            Err(e) => panic!("{e}")
+        }
+    }
+
+    /* Save / Getter */
+    pub fn save(&self) -> io::Result<()> {
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open(PATH_TO_SAVE)?;
+
+        let encoded = match self.encode() {
+            Some(e) => e,
+            None => return Err(io::Error::new(io::ErrorKind::Other, "Failed to encode board."))
+        };
+
+        file.write(&encoded)?;
+        Ok(())
+    }
+    pub fn from_save() -> io::Result<Self> {
+        let mut file = File::open(PATH_TO_SAVE)?;
+
+        /* Pre-alloc */
+        let mut buf:Vec<u8> = Vec::new();
+        
+        /* Return */
+        file.read_to_end(&mut buf)?;
+        match Self::decode(&buf) {
+            Some(e) => Ok(e),
+            None => Err(io::Error::new(io::ErrorKind::Other, "Failed to decode board."))
+        }
     }
 }
